9c012ee4f2a3439abf37b4c67bdecb4d42b0dc9d
w_caoping <w_caoping@xiwang.com>
2024-11-08 19:04:03 +0800

    fix

--- jcy/common/main.py
+++ jcy/common/main.py
@@ -39,10 +39,12 @@ def parse_face_alarm(data):
             for face_alarm in face_alarms:
                 face_id = face_alarm["Id"]
                 if face_id > 0:
-                    logger.info(f"face_alarm={json.dumps(face_alarm)}")
-                    face_name = face_alarm["Name"]
-                    face_phone = face_alarm["Phone"]
-                    check_time_alert(face_name, face_phone, alarm_time)
+                    try:
+                        face_name = face_alarm["Name"]
+                        face_phone = face_alarm["Phone"]
+                        check_time_alert(face_name, face_phone, alarm_time)
+                    except Exception as exp:
+                        logger.error(f"check first failed: {exp}")
 
 
 def check_time_alert(face_name, face_phone, alarm_time):
